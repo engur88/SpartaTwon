ec88c29d617a92e9850748070700923e995fa220
engur88 <thsengur88@naver.com>
2024-05-14 11:45:05 +0900

    [feat]

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public float cameraSpeed;   // cameraSpeed 값에 따라서 카메라 이동속도가 결정됨
+
+    private void LateUpdate()   // LateUpdate는 Update함수가 호출된 후에 호출됨
+    {
+        //transform.position = new Vector3(player.position.x, player.position.y, -10f);
+        // Vector3Lerp는 A와 B사이의 벡터 값을 반환
+        transform.position = Vector3.Lerp(transform.position, player.position, Time.deltaTime * cameraSpeed);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10f);  // 카메라의 Z값을 -10f로 고정
+    }
+}
---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : TopDownController
+{
+    private Camera camera;
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveIput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveIput);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 WorldPos = camera.ScreenToWorldPoint(newAim);
+        newAim = (WorldPos - (Vector2)transform.position).normalized;
+
+        CallLookEvent(newAim);
+    }
+
+    public void OnFire(InputValue value)
+    {
+        IsAttacking = value.isPressed;
+    }
+}
